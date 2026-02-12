55d0ddeecb7d95c5f6a1c99944d7eae2ac86e2ac
jungit66 <oizumiis2024@gmail.com>
2026-02-12 12:43:05 +0900

    Changing Model to 2.5-lite

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title="Note RAG", page_icon="📝")
 @st.cache_resource
 def load_index():
     #モデルの設定
-    Settings.llm = Gemini(model="gemini-2.5-flash", temperature=0.5)
+    Settings.llm = Gemini(model="gemini-2.5-flash-lite", temperature=0.5)
 
     embed_model = HuggingFaceEmbedding(model_name="intfloat/multilingual-e5-large")
 
